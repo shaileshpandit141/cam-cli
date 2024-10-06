9f980935de6c581f83bf344f4409e473d9f89276
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-10-06 20:19:12 +0530

    Add format function to format text and add some helpful seggetions for cli consile

--- build/lib/cam/controller.py
+++ build/lib/cam/controller.py
@@ -2,6 +2,7 @@ from sys import argv
 from cam.command.commad import get_commans
 from cam.create_app.create_app import CreateApp
 from cam.run.run import run
+from cam.utils.format import format
 from cam.app_tree.java_script_tree import java_script_tree
 
 
@@ -10,14 +11,51 @@ def controller():
     absolute_path, command_name, command_options = get_commans()
 
     if command_name is None:
-        return "Invalid command name. did you mean xyz"
+        return format(f"""
+            {"Help".center(50, "-")}
+
+            :: cam  <- command
+
+            cam command Required at list one argument.
+
+            List of supported argument are:
+
+            :: create-js-app    <- for creating javascript application.
+            :: create-py-module    <- for creating python module.
+            :: create-flask-app    <- for creating flask application.
+
+            Example:
+            create an app with specific app name.
+            :: cam create-js-app <app_name>
+            :: cam create-py-module <module_name>
+            :: cam create-flask-app <app_name>
+            
+            create an app with in the current directiory.
+            :: cam create-js-app .
+            :: cam create-py-module .
+            :: cam create-flask-app .
+
+            {"-".center(50, "-")}
+        """)
 
     # Command validation.
     if command_name.lower() == "create-js-app":
         if len(command_options) <=0:
-            return "Oop's app_name can not be empty enter . or other valid name."
+            return format(f"""
+                {" Error ".center(50, "-")}
 
-        # Perform your actions.
+                :: cam create-js-app    <- command
+
+                cam create-js-app command required an app_name or . for current directory.
+
+                Example:
+                :: cam create-js-app <app_name>    <- create an app with specific app name.
+                :: cam create-js-app .    <- create an app with in the current directiory.
+
+                {"-".center(50, "-")}
+            """)
+
+        # Perform creation of javascript app.
         app_name = command_options[0]
         try:
             js_app = CreateApp(app_name, java_script_tree())
@@ -29,10 +67,24 @@ def controller():
 
     elif command_name.lower() == "create-py-module":
         if len(command_options) <=0:
-            return "Oop's app_name can not be empty enter . or other valid name."
+            return format(f"""
+                {" Error ".center(50, "-")}
+
+                :: cam create-py-module    <- command
+
+                cam create-py-module command required an module_name or . for current directory.
+
+                Example:
+                :: cam create-py-module .    <- create an app with in the current directiory.
+                :: cam create-py-module <module_name>    <- create an app with specific module name.
+
+                {"-".center(50, "-")}
+            """)
+
+        # Perform creation of javascript app.
         print("py module created.")
-    
-    elif command_name.lower() == 'run': 
+
+    elif command_name.lower() == 'run':
         response = run()
         return response
     else:
