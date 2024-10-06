aaf2b81e9704745cd683f0c11eec7e604ce12f1c
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-10-06 22:21:50 +0530

    Publish on pypi.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="cam-cli",
-    version="0.1.0",
+    name="cam-cli-tool",
+    version="0.1.1",
     author="shailesh",
     author_email="shaileshpandit141@gmail.com.com",
     description=("""
