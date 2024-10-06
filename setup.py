cef0197b9fb90a57c727653902c9791e954ae12f
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-10-06 20:44:31 +0530

    Update the setup.py file for production.

--- setup.py
+++ setup.py
@@ -1,24 +1,35 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="mycliapp",
-    version="0.1",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "cam=cam.main:main",  # CLI command
-        ],
-    },
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A CLI app with math and string utilities using sys module",
-    long_description="",
+    name="cam-cli",
+    version="0.1.0",
+    author="shailesh",
+    author_email="shaileshpandit141@gmail.com.com",
+    description=("""
+        cam CLI is a command-line tool for creating boilerplate code for various types of applications
+        including vanilla JavaScript frontend web app, Python module, Flask app and etc.
+    """),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/mycliapp",
+    url="https://github.com/shaileshpandit141/cam-cli",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires='>=3.8',
+    install_requires=[
+        "livereload>=2.5"
+    ],
+    entry_points={
+        "console_scripts": [
+            "cam=cam.main:main",  # cam CLI command.
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
 )
