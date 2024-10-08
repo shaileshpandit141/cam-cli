aee8c90c2c4fb971edc9757ebeae161299dba647
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-10-08 18:56:15 +0530

    Update the default app javascript in js app

--- build/lib/cam/app_tree/templates/javascript_template.py
+++ build/lib/cam/app_tree/templates/javascript_template.py
@@ -11,6 +11,7 @@ def camconfig_json() -> str:
                 "watch": "."
             }
         }
+        
         ''')
 
 def index_html() -> str:
@@ -40,6 +41,7 @@ def index_html() -> str:
             <script type="module" src="./src/app.js"></script>
         </body>
         </html>
+        
         ''')
 
 def favicon_svg() -> str:
@@ -48,6 +50,7 @@ def favicon_svg() -> str:
         <rect width="630" height="630" fill="#f7df1e"/>
         <path d="m423.2 492.19c12.69 20.72 29.2 35.95 58.4 35.95 24.53 0 40.2-12.26 40.2-29.2 0-20.3-16.1-27.49-43.1-39.3l-14.8-6.35c-42.72-18.2-71.1-41-71.1-89.2 0-44.4 33.83-78.2 86.7-78.2 37.64 0 64.7 13.1 84.2 47.4l-46.1 29.6c-10.15-18.2-21.1-25.37-38.1-25.37-17.34 0-28.33 11-28.33 25.37 0 17.76 11 24.95 36.4 35.95l14.8 6.34c50.3 21.57 78.7 43.56 78.7 93 0 53.3-41.87 82.5-98.1 82.5-54.98 0-90.5-26.2-107.88-60.54zm-209.13 5.13c9.3 16.5 17.76 30.45 38.1 30.45 19.45 0 31.72-7.61 31.72-37.2v-201.3h59.2v202.1c0 61.3-35.94 89.2-88.4 89.2-47.4 0-74.85-24.53-88.81-54.075z"/>
         </svg>
+        
         ''')
 
 def index_css() -> str:
@@ -177,6 +180,7 @@ def index_css() -> str:
             align-items: center;
             justify-content: center;
         }
+        
         ''')
 
 def app_css() -> str:
@@ -257,6 +261,7 @@ def app_css() -> str:
         .button:hover {
           background-color: #bbbbbb;
         }
+
         ''')
 
 def app_js() -> str:
@@ -264,7 +269,7 @@ def app_js() -> str:
         const root = document.getElementById('root')
 
         document.addEventListener('DOMContentLoaded', () => {
-          root.innerHTML = ((() => (
+          root.insertAdjacentHTML("beforeend", (() => (
             `
             <div class="app">
               <div class="logo">00</div>
@@ -283,4 +288,5 @@ def app_js() -> str:
             logo.innerHTML = initialNum < 9 ? `0${initialNum + 1}` : initialNum + 1
           })
         })
+
         ''')
