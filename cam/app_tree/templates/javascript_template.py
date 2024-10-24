691d907c6ba3f0ffe0f28f396f5a1d6aebfb694f
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-10-24 19:07:01 +0530

    Update the templates of javascript_template

--- cam/app_tree/templates/javascript_template.py
+++ cam/app_tree/templates/javascript_template.py
@@ -11,7 +11,7 @@ def camconfig_json() -> str:
                 "watch": "."
             }
         }
-        
+
         ''')
 
 def index_html() -> str:
@@ -29,8 +29,8 @@ def index_html() -> str:
             <title>cam cli</title>
 
             <!-- Linked stylesheet files. -->
-            <link rel="stylesheet" href="./styles/app.css">
             <link rel="stylesheet" href="./styles/index.css">
+            <link rel="stylesheet" href="./styles/app.css">
         </head>
         <body>
             <div id="root">
@@ -41,7 +41,7 @@ def index_html() -> str:
             <script type="module" src="./src/app.js"></script>
         </body>
         </html>
-        
+
         ''')
 
 def favicon_svg() -> str:
@@ -50,7 +50,7 @@ def favicon_svg() -> str:
         <rect width="630" height="630" fill="#f7df1e"/>
         <path d="m423.2 492.19c12.69 20.72 29.2 35.95 58.4 35.95 24.53 0 40.2-12.26 40.2-29.2 0-20.3-16.1-27.49-43.1-39.3l-14.8-6.35c-42.72-18.2-71.1-41-71.1-89.2 0-44.4 33.83-78.2 86.7-78.2 37.64 0 64.7 13.1 84.2 47.4l-46.1 29.6c-10.15-18.2-21.1-25.37-38.1-25.37-17.34 0-28.33 11-28.33 25.37 0 17.76 11 24.95 36.4 35.95l14.8 6.34c50.3 21.57 78.7 43.56 78.7 93 0 53.3-41.87 82.5-98.1 82.5-54.98 0-90.5-26.2-107.88-60.54zm-209.13 5.13c9.3 16.5 17.76 30.45 38.1 30.45 19.45 0 31.72-7.61 31.72-37.2v-201.3h59.2v202.1c0 61.3-35.94 89.2-88.4 89.2-47.4 0-74.85-24.53-88.81-54.075z"/>
         </svg>
-        
+
         ''')
 
 def index_css() -> str:
@@ -180,7 +180,7 @@ def index_css() -> str:
             align-items: center;
             justify-content: center;
         }
-        
+
         ''')
 
 def app_css() -> str:
@@ -192,74 +192,74 @@ def app_css() -> str:
         }
         /* app initial test styles */
         .app {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          user-select: none;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            user-select: none;
         }
 
         /* Circle logo styles */
         .logo {
-          aspect-ratio: 1/1;
-          width: 100%;
-          max-width: 350px;
-          background-color: #cccccc;
-          border-radius: 100%;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          letter-spacing: 0.04em;
-          animation: scaleUpDown 2s infinite;
+            aspect-ratio: 1/1;
+            width: 100%;
+            max-width: 350px;
+            background-color: #cccccc;
+            border-radius: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            letter-spacing: 0.04em;
+            animation: scaleUpDown 2s infinite;
         }
 
         /* Keyframes for scale up and down */
         @keyframes scaleUpDown {
-          0% {
-            transform: scale(.8);
-          }
-
-          50% {
-            transform: scale(1);
-          }
-
-          100% {
-            transform: scale(.8);
-          }
+            0% {
+                transform: scale(.8);
+            }
+    
+            50% {
+                transform: scale(1);
+            }
+    
+            100% {
+                transform: scale(.8);
+            }
         }
 
         h1 {
-          padding-block: 16px;
-          letter-spacing: 0.04em;
-          text-align: center;
-          max-width: 550px;
+            padding-block: 16px;
+            letter-spacing: 0.04em;
+            text-align: center;
+            max-width: 550px;
         }
 
         p {
-          letter-spacing: 0.04em;
-          line-height: 1.5;
-          text-align: center;
-          width: 100%;
-          max-width: 450px;
+            letter-spacing: 0.04em;
+            line-height: 1.5;
+            text-align: center;
+            width: 100%;
+            max-width: 450px;
         }
 
         /* Button styles */
         .button {
-          background-color: #cccccc;
-          margin-block: 16px;
-          height: 40px;
-          padding-inline: 16px;
-          border-radius: 999rem;
-          font-size: 16px;
-          font-weight: 600;
-          letter-spacing: 0.04em;
-          text-transform: capitalize;
-          transition: background-color 300ms ease-in-out;
+            background-color: #cccccc;
+            margin-block: 16px;
+            height: 40px;
+            padding-inline: 16px;
+            border-radius: 999rem;
+            font-size: 16px;
+            font-weight: 600;
+            letter-spacing: 0.04em;
+            text-transform: capitalize;
+            transition: background-color 300ms ease-in-out;
         }
 
         .button:hover {
-          background-color: #bbbbbb;
+            background-color: #bbbbbb;
         }
 
         ''')
